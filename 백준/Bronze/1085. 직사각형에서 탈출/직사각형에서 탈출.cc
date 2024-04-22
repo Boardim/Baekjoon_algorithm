4216e3942fb6b5079dcbe70e1f99284068cecf4c
Boardim <80339960+Boardim@users.noreply.github.com>
2024-04-22 22:41:58 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+	if (w - x < x) x = w - x;
+	if (h - y < y) y = h - y;
+	if (x > y) cout << y;
+	else cout << x;
+}
