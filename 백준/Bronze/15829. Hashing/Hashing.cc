3c1222a7153ceadcffbe834dd0db8cd222a66dc3
Boardim <80339960+Boardim@users.noreply.github.com>
2024-05-02 19:09:48 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main() {
 	string a;
-	int l, sum{ 0 };
+	long long int l, sum{ 0 }, x{ 1 };
 	cin >> l >> a;
 	for (int i = 0; i < l; i++) {
-		sum += (a[i] - 96) * static_cast<int>(pow(31, i)) % 1234567891;
+		sum = (sum + (a[i] - 96) * x) % 1234567891;
+		x = (x * 31) % 1234567891;
 	}
 	cout << sum;
 }
